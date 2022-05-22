4bb9ccbe3e53ec23555ffe83bf32bf15d5cace67
Hari om  jee <81242043+hariomjee@users.noreply.github.com>
2022-05-22 22:44:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$databasename = "course_registration"; 
+$databasetable = "student_details"; 
+
+$fieldseparator = ","; 
+$lineseparator = "\n";
+
+$csvfile = "data.csv";
+
+
+if(!file_exists($csvfile)) 
+{
+  die("File not found. Make sure you specified the correct path.");
+  
+}
+
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$databasename", $username, $password,
+  // set the PDO error mode to exception
+  array
+  (
+    "PDO::MYSQL_ATTR_LOCAL_INFILE => true,PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION"
+  )
+  );
+  
+  "Connected successfully";
+} 
+catch (PDOException $e) 
+{
+  die("database connection failed: ".$e->getMessage());
+}
+
+
+$affectedRows = $conn->exec
+(
+  "LOAD DATA LOCAL INFILE "
+  ".$conn->quote($csvfile)",
+  ." INTO TABLE `$databasetable` FIELDS TERMINATED BY "
+  .$conn->quote($fieldseparator)
+  ."LINES TERMINATED BY "
+  .$conn->quote($lineseparator)
+);
---
+++ index.php
@@ -0,0 +1,14 @@
+
+<?php include('database.php'); ?>
+<!DOCTYPE html>
+<html>
+<head></head>
+<body>
+<div class="upload-form">
+<form  method="post" enctype="multipart/form-data">
+    <input type="file" name="file_name[]" multiple>
+    <input type="submit" value="Upload File" name="submit">
+</form>
+</div>
+</body>
+</html>
